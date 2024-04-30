7c8ecbb77cc879775964b904aa58e631cddaffb1
nickamirul <nickamirul9945@gmail.com>
2024-04-30 10:35:44 +0800

    add http request to store cart data to firebase

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import { useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import { useEffect } from 'react';
 
 function App() {
 
-  const showCart = useSelector(state => state.showCart.showCart)
+  const showCart = useSelector((state) => state.showCart.showCart);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch('https://redux-practice-6b085-default-rtdb.firebaseio.com/cart.json', { method: 'PUT', body: JSON.stringify(cart) });
+  }, [cart])
 
   return (
     <Layout>
